e2c227f2e04b5cfb73b1b8f5f2ff0cb5158f1ec8
sujansarkar69 <tamim1995.itk@gmail.com>
2023-02-14 01:49:28 +0600

    first commit

---
+++ week 08/Module 29.5 practice day 1/2, bfs, in, pre, post order.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int id;
+    int value;
+    node *left;
+    node *right;
+    node *parent;
+};
+
+class BinaryTree
+{
+public:
+    node *root;
+    BinaryTree()
+    {
+        root = NULL;
+    }
+
+    node *CreateNewNode(int id, int value)
+    {
+        node *newnode = new node;
+        newnode->id = id;
+        newnode->value = value;
+        newnode->left = NULL;
+        newnode->right = NULL;
+        newnode->parent = NULL;
+        return newnode;
+    }
+
+    void Insert(int id, int value)
+    {
+        node *newnode = CreateNewNode(id, value);
+        if (root == NULL)
+        {
+            root = newnode;
+            return;
+        }
+        queue<node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            node *a = q.front();
+            q.pop();
+            if (a->left != NULL)
+            {
+                q.push(a->left);
+            }
+            else
+            {
+                a->left = newnode;
+                newnode->parent = a;
+                return;
+            }
+            if (a->right != NULL)
+            {
+                q.push(a->right);
+            }
+            else
+            {
+                a->right = newnode;
+                newnode->parent = a;
+                return;
+            }
+        }
+    }
+
+    void BFS()
+    {
+        if (root == NULL)
+            return;
+        queue<node *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int l = -1, r = -1, p = -1;
+            node *a = q.front();
+            q.pop();
+            if (a->parent != NULL)
+            {
+                p = a->parent->id;
+            }
+            if (a->left != NULL)
+            {
+                l = a->left->id;
+                q.push(a->left);
+            }
+            if (a->right != NULL)
+            {
+                r = a->right->id;
+                q.push(a->right);
+            }
+
+            cout << "Node " << a->id << ": Parent = " << p << ", Left child = " << l << ", Right child = " << r << "\n";
+        }
+    }
+
+    void inorder(node *a)
+    {
+        if (a == NULL)
+            return;
+        inorder(a->left);
+        cout << a->id << " ";
+        inorder(a->right);
+    }
+
+    void preorder(node *a)
+    {
+        if (a == NULL)
+            return;
+        cout << a->id << " ";
+        preorder(a->left);
+        preorder(a->right);
+    }
+
+    void postorder(node *a)
+    {
+        if (a == NULL)
+            return;
+        postorder(a->left);
+        postorder(a->right);
+        cout << a->id << " ";
+    }
+};
+int main()
+{
+
+    BinaryTree bt;
+    bt.Insert(0, 5);
+    bt.Insert(1, 10);
+    bt.Insert(5, 23);
+    bt.BFS();
+    bt.inorder(bt.root);
+    cout<<"\n";
+    bt.preorder(bt.root);
+    cout<<"\n";
+    bt.postorder(bt.root);
+
+    return 0;
+}
---
+++ week 08/Module 29.5 practice day 1/3. height of binary tree.cpp
@@ -0,0 +1,162 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int id;
+    int value;
+    node *left;
+    node *right;
+    node *parent;
+};
+
+class BinaryTree
+{
+public:
+    node *root;
+    BinaryTree()
+    {
+        root = NULL;
+    }
+
+    node *CreateNewNode(int id, int value)
+    {
+        node *newnode = new node;
+        newnode->id = id;
+        newnode->value = value;
+        newnode->left = NULL;
+        newnode->right = NULL;
+        newnode->parent = NULL;
+        return newnode;
+    }
+
+    void Insert(int id, int value)
+    {
+        node *newnode = CreateNewNode(id, value);
+        if (root == NULL)
+        {
+            root = newnode;
+            return;
+        }
+        queue<node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            node *a = q.front();
+            q.pop();
+            if (a->left != NULL)
+            {
+                q.push(a->left);
+            }
+            else
+            {
+                a->left = newnode;
+                newnode->parent = a;
+                return;
+            }
+            if (a->right != NULL)
+            {
+                q.push(a->right);
+            }
+            else
+            {
+                a->right = newnode;
+                newnode->parent = a;
+                return;
+            }
+        }
+    }
+
+    void BFS()
+    {
+        if (root == NULL)
+            return;
+        queue<node *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int l = -1, r = -1, p = -1;
+            node *a = q.front();
+            q.pop();
+            if (a->parent != NULL)
+            {
+                p = a->parent->id;
+            }
+            if (a->left != NULL)
+            {
+                l = a->left->id;
+                q.push(a->left);
+            }
+            if (a->right != NULL)
+            {
+                r = a->right->id;
+                q.push(a->right);
+            }
+
+            cout << "Node " << a->id << ": Parent = " << p << ", Left child = " << l << ", Right child = " << r << "\n";
+        }
+    }
+
+    void inorder(node *a)
+    {
+        if (a == NULL)
+            return;
+        inorder(a->left);
+        cout << a->id << " ";
+        inorder(a->right);
+    }
+
+    void preorder(node *a)
+    {
+        if (a == NULL)
+            return;
+        cout << a->id << " ";
+        preorder(a->left);
+        preorder(a->right);
+    }
+
+    void postorder(node *a)
+    {
+        if (a == NULL)
+            return;
+        postorder(a->left);
+        postorder(a->right);
+        cout << a->id << " ";
+    }
+
+    int height(node *a)
+    {
+        if (a == NULL)
+            return 0;
+
+        int lheight = height(a->left);
+        int rheight = height(a->right);
+
+        if (lheight > rheight)
+        {
+            return (lheight + 1);
+        }
+        else
+        {
+            return (rheight + 1);
+        }
+    }
+};
+int main()
+{
+    BinaryTree bt;
+    bt.Insert(0, 5);
+    bt.Insert(1, 10);
+    bt.Insert(2, 23);
+    bt.Insert(3, 30);
+    bt.Insert(4, 5);
+    bt.Insert(5, 10);
+    bt.Insert(6, 23);
+   
+    bt.BFS();
+    cout << bt.height(bt.root);
+
+    return 0;
+}
